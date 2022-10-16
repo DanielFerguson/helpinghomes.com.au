f83cf03cf89c6440c52f04b5fc35a2bb1a954d48
Daniel Ferguson <thedanielfergusonkid@gmail.com>
2022-10-16 22:44:10 +1100

    removed welcome blade page

--- routes/api.php
+++ routes/api.php
@@ -8,4 +8,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// TODO: Protect create, edit, delete route with middleware
 Route::apiResource('offers', OfferController::class);
