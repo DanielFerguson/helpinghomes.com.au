7a14745841e617acd4623eadfd71fa0e7e4e9431
Daniel Ferguson <thedanielfergusonkid@gmail.com>
2022-10-16 22:41:35 +1100

    extracted auth logic to controller

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+
+class AuthController extends Controller
+{
+    public function redirectApple()
+    {
+        return Socialite::driver('apple')->redirect();
+    }
+
+    // TODO
+    public function callbackApple()
+    {
+        $appleUser = Socialite::driver('apple')->user();
+
+        $user = User::updateOrCreate([
+            'apple_id' => $appleUser->id,
+        ], [
+            'name' => $appleUser->name,
+            'email' => $appleUser->email,
+            'apple_token' => $appleUser->token,
+            'apple_refresh_token' => $appleUser->refreshToken,
+        ]);
+
+        Auth::login($user);
+
+        return redirect('/');
+    }
+
+    public function redirectGithub()
+    {
+        return Socialite::driver('github')->redirect();
+    }
+
+    public function callbackGithub()
+    {
+        $githubUser = Socialite::driver('github')->user();
+
+        $user = User::updateOrCreate([
+            'github_id' => $githubUser->id,
+        ], [
+            'name' => $githubUser->name,
+            'email' => $githubUser->email,
+            'github_token' => $githubUser->token,
+            'github_refresh_token' => $githubUser->refreshToken,
+        ]);
+
+        Auth::login($user);
+
+        return redirect('/');
+    }
+
+    public function redirectGoogle()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function callbackGoogle()
+    {
+        $googleUser = Socialite::driver('google')->user();
+
+        $user = User::updateOrCreate([
+            'google_id' => $googleUser->id,
+        ], [
+            'name' => $googleUser->name,
+            'email' => $googleUser->email,
+            'google_token' => $googleUser->token,
+            'google_refresh_token' => $googleUser->refreshToken,
+        ]);
+
+        Auth::login($user);
+
+        return redirect('/');
+    }
+
+    public function redirectFacebook()
+    {
+        return Socialite::driver('facebook')->redirect();
+    }
+
+    public function callbackFacebook()
+    {
+        $facebookUser = Socialite::driver('facebook')->user();
+
+        $user = User::updateOrCreate([
+            'facebook_id' => $facebookUser->id,
+        ], [
+            'name' => $facebookUser->name,
+            'email' => $facebookUser->email,
+            'facebook_token' => $facebookUser->token,
+            'facebook_refresh_token' => $facebookUser->refreshToken,
+        ]);
+
+        Auth::login($user);
+
+        return redirect('/');
+    }
+
+    public function redirectTwitter()
+    {
+        return Socialite::driver('twitter')->redirect();
+    }
+
+    public function callbackTwitter()
+    {
+        $twitterUser = Socialite::driver('twitter')->user();
+
+        $user = User::updateOrCreate([
+            'twitter_id' => $twitterUser->id,
+        ], [
+            'name' => $twitterUser->name,
+            'email' => $twitterUser->email,
+            'twitter_token' => $twitterUser->token,
+            'twitter_refresh_token' => $twitterUser->refreshToken,
+        ]);
+
+        Auth::login($user);
+
+        return redirect('/');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,10 +14,20 @@ use Inertia\Inertia;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::inertia('/', 'home');
 
-Route::get('/test', function () {
-    return Inertia::render('home');
-});
+/*
+|--------------------------------------------------------------------------
+| Authentication Routes
+|--------------------------------------------------------------------------
+*/
+
+Route::get('/auth/redirect/github', [AuthController::class, 'redirectGithub']);
+Route::get('/auth/redirect/google', [AuthController::class, 'redirectGoogle']);
+Route::get('/auth/redirect/facebook', [AuthController::class, 'redirectFacebook']);
+Route::get('/auth/redirect/twitter', [AuthController::class, 'redirectTwitter']);
+
+Route::get('/auth/callback/github', [AuthController::class, 'callbackGithub']);
+Route::get('/auth/callback/google', [AuthController::class, 'callbackGoogle']);
+Route::get('/auth/callback/facebook', [AuthController::class, 'callbackFacebook']);
+Route::get('/auth/callback/twitter', [AuthController::class, 'callbackTwitter']);
