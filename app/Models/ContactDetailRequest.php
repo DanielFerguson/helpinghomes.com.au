b0bd59c971568378663a2862c1bb2f3a61e19fa5
Daniel Ferguson <thedanielfergusonkid@gmail.com>
2022-10-18 00:34:40 +1100

    log contact detail requests

---
+++ app/Models/ContactDetailRequest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class ContactDetailRequest extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'offer_id'
+    ];
+
+    public function requester(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function offer(): BelongsTo
+    {
+        return $this->belongsTo(Offer::class);
+    }
+}
