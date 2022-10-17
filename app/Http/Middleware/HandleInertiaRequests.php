ca0380cb64ed238973f9eeaad8e705d02e1486d7
Daniel Ferguson <thedanielfergusonkid@gmail.com>
2022-10-17 14:28:16 +1100

    returning null on false, rather than null

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -39,7 +39,7 @@ class HandleInertiaRequests extends Middleware
         return array_merge(parent::share($request), [
             'auth.user' => fn () => $request->user()
                 ? $request->user()->only('id', 'name')
-                : null,
+                : false,
         ]);
     }
 }
