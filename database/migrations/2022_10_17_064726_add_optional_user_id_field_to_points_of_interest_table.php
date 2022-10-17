5bddd09f5a887b16ee33b53e27eb0a813dd06c06
Daniel Ferguson <thedanielfergusonkid@gmail.com>
2022-10-17 19:57:47 +1100

    updated method of tracking offers, implemented soft deletes

---
+++ database/migrations/2022_10_17_064726_add_optional_user_id_field_to_points_of_interest_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('point_of_interests', function (Blueprint $table) {
+            $table->uuid('user_id')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('point_of_interests', function (Blueprint $table) {
+            $table->dropColumn('user_id');
+        });
+    }
+};
