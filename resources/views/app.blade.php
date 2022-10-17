533c8370db5d832b7c234652f1b1340836435836
Daniel Ferguson <thedanielfergusonkid@gmail.com>
2022-10-18 00:35:15 +1100

    added basic seo meta tags

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,6 @@
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-  <title>Helping Homes</title>
 
   <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
   <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
@@ -13,6 +12,24 @@
   <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
   <meta name="msapplication-TileColor" content="#da532c">
   <meta name="theme-color" content="#ffffff">
+
+  <title>Helping Homes — Safer, together.</title>
+  <meta name="title" content="Helping Homes — Safer, together.">
+  <meta name="description" content="Emergency accommodation, offers of assistance, livestock transport and accommodation. An open service to connect those that need emergency housing, with the people who can provide it. ">
+
+  <!-- Open Graph / Facebook -->
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="https://www.helpinghomes.com.au">
+  <meta property="og:title" content="Helping Homes — Safer, together.">
+  <meta property="og:description" content="Emergency accommodation, offers of assistance, livestock transport and accommodation. An open service to connect those that need emergency housing, with the people who can provide it. ">
+  <!-- <meta property="og:image" content="https://www.helpinghomes.com.auassets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"> -->
+
+  <!-- Twitter -->
+  <meta property="twitter:card" content="summary_large_image">
+  <meta property="twitter:url" content="https://www.helpinghomes.com.au">
+  <meta property="twitter:title" content="Helping Homes — Safer, together.">
+  <meta property="twitter:description" content="Emergency accommodation, offers of assistance, livestock transport and accommodation. An open service to connect those that need emergency housing, with the people who can provide it. ">
+  <!-- <meta property="twitter:image" content="https://www.helpinghomes.com.auassets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"> -->
 
   <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
   <script src="{{ mix('/js/app.js') }}" defer></script>
